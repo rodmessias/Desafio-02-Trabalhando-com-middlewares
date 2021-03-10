062b6d46d5098c04e245fee2ab63f600fd03ff25
rodmessias <rodmessias82@gmail.com>
2021-03-10 11:42:24 -0300

    desafio 2 completo

--- src/index.js
+++ src/index.js
@@ -11,18 +11,81 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe!' })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length < 10) {
+    return next();
+  }
+
+  if (user.pro) {
+    return next();
+  }
+
+  return response.status(403).json({ error: 'Limite do plano atingido' })
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.username === username  
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe' })
+  }
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  )
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Todo não existe' })
+  }
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo não existe' })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.id === id  
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe' })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
